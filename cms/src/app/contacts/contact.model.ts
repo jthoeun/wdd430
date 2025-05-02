d0176f1bfc754e2a4f982f2d840c7ec047580183
jthoeun <johnny.thoeun@gmail.com>
2025-05-02 11:09:11 -0700

    Week 2- CMS inital commit

---
+++ cms/src/app/contacts/contact.model.ts
@@ -0,0 +1,17 @@
+export class Contact {
+    public id: number;
+    public name: string;
+    public email: string;
+    public phone: number;
+    public imageURL: string;
+    public group: Contact[];
+
+    constructor(id: number, name: string, email: string, phone: number, imageURL: string, group: Contact[] = []){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.imageURL = imageURL;
+        this.group = group;
+    }
+}
