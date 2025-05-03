a623b2020e5a56d8f746c74083862e896d1ed6a5
jthoeun <johnny.thoeun@gmail.com>
2025-05-02 21:01:34 -0700

    Week 2- More CMS updates

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
 @Component({
   selector: 'cms-contact-detail',
@@ -6,6 +6,11 @@ import { Contact } from '../contact.model';
   templateUrl: './contact-detail.component.html',
   styleUrl: './contact-detail.component.css'
 })
-export class ContactDetailComponent {
-  @Input() contact!: Contact;
+export class ContactDetailComponent implements OnInit {
+  contact?: Contact;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
