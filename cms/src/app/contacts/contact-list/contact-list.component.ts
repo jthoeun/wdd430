91938b20eadac5ba5c1e6adfde2cf7bbf1878591
jthoeun <johnny.thoeun@gmail.com>
2025-05-20 21:12:21 -0600

    Week 5 CMS - Contacts Services

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-list',
@@ -9,18 +10,15 @@ import { Contact } from '../contact.model';
 })
 export class ContactListComponent implements OnInit{
   
-  contacts: Contact[] = [
-    new Contact(1, 'R.Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', 'assets/images/jacksonk.jpg'),
-    new Contact(2, 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', 'assets/images/barzeer.jpg')
-  ];
-    @Output() selectedContactEvent = new EventEmitter<Contact>();
-    constructor () {}
+  contacts: Contact[] = [];
+    
+  constructor (private contactService: ContactService) {}
   
-    ngOnInit() {
-
-    }
+  ngOnInit() {
+    this.contacts = this.contactService.getContacts();
+  }
 
   onSelected(contact: Contact){
-    this.selectedContactEvent.emit(contact);
+    this.contactService.contactSelectedEvent.emit(contact);
   }
 }
--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -1,17 +1,10 @@
-export class Contact {
-    public id: number;
-    public name: string;
-    public email: string;
-    public phone: string;
-    public imageURL: string;
-    public group: Contact[]
-
-    constructor(id: number, name: string, email: string, phone: string, imageURL: string, group: Contact[] = []){
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.imageURL = imageURL;
-        this.group = group;
-    }
-}
+  export class Contact {
+     constructor (
+        public id: string,
+        public name: string,
+        public email: string,
+        public phone: string,
+        public imageURL: string,
+        public group?: Contact[] | null
+     ) {}
+  }
