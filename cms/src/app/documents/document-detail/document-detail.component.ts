7e3de565ae9cccc2b1c85ee49db15bce34b8b99a
jthoeun <johnny.thoeun@gmail.com>
2025-05-29 22:54:11 -0600

    WK6 CMS Routes Document Delete

--- cms/src/app/documents/document-detail/document-detail.component.ts
+++ cms/src/app/documents/document-detail/document-detail.component.ts
@@ -33,5 +33,10 @@ export class DocumentDetailComponent implements OnInit {
     if (this.document.url) this.nativeWindow.open(this.document.url);
   }
 
+  onDelete() {
+    this.docService.deleteDocument(this.document);
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
+
 
 }
