3261487404980771fab875b03b882e0d2c4c3279
jthoeun <johnny.thoeun@gmail.com>
2025-06-05 21:11:39 -0600

    WK7 Obervalbes update; contacts & minor revision on document

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,6 +1,7 @@
-import { Component,  OnInit } from '@angular/core';
+import { Component,  OnDestroy,  OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
 import { ContactService } from '../contact.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'cms-contact-list',
@@ -8,17 +9,22 @@ import { ContactService } from '../contact.service';
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.css'
 })
-export class ContactListComponent implements OnInit{
+export class ContactListComponent implements OnInit, OnDestroy{
   
   contacts: Contact[] = [];
+  subsciption!: Subscription;
     
   constructor (private contactService: ContactService) {}
   
   ngOnInit() {
     this.contacts = this.contactService.getContacts();
-    this.contactService.contactChangedEvent.subscribe((contacts: Contact[]) => {
+    this.subsciption = this.contactService.contactListChangedEvent.subscribe((contacts: Contact[]) => {
       this.contacts = contacts;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subsciption.unsubscribe();
   }
 
 }
