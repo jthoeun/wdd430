b887823d749fe53089e54046409c59f394d77616
jthoeun <johnny.thoeun@gmail.com>
2025-06-18 20:09:28 -0600

    Wk9 Firebase support for Contact and Message services

--- cms/src/app/contacts/contact.service.ts
+++ cms/src/app/contacts/contact.service.ts
@@ -1,45 +1,54 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { Contact } from './contact.model';
-import { MOCKCONTACTS } from './MOCKCONTACTS';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { Contact } from './contact.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactService {
   contactSelectedEvent = new EventEmitter<Contact>();
   contactListChangedEvent = new Subject<Contact[]>();
-
+  private contactsUrl =
+    'https://wdd430-cms-2309e-default-rtdb.firebaseio.com/contacts.json';
   private contacts: Contact[] = [];
-  private maxContactId = this.getMaxId();
+  private maxContactId: number = 0;
 
-  constructor() { 
-    this.contacts= MOCKCONTACTS;
-    this.maxContactId = this.getMaxId();
+  constructor(private http: HttpClient) {
+    this.getContacts();
   }
 
+  // GET REQUEST
   getContacts(): Contact[] {
+    this.http
+      .get<Contact[]>(this.contactsUrl)
+      .subscribe((contacts: Contact[]) => {
+        this.contacts = contacts;
+        this.maxContactId = this.getMaxId();
+        this.contacts.sort((a, b) => {
+          if (a < b) return -1;
+          if (a > b) return 1;
+          return 0;
+        });
+        this.contactListChangedEvent.next(this.contacts.slice());
+      });
     return this.contacts.slice();
   }
 
-  getContact(id: string): Contact | undefined {
-    return this.contacts.find((c) => c.id === id)!;
-  }
-
-  deleteContact(contact: Contact) {
-    if(!contact) return;
-    const pos = this.contacts.indexOf(contact);
-    if (pos < 0) return;
-    this.contacts.splice(pos, 1);
-    this.contactListChangedEvent.next(this.contacts.slice());
-  }
-
-  getMaxId(): number {
-    let maxId = 0;
-    this.contacts.forEach((c) =>{
-      if (+c.id > maxId) maxId = +c.id;
-    });
-    return maxId;
+  // PUT REQUEST
+  storeContacts() {
+    this.http
+      .put(this.contactsUrl, JSON.stringify(this.contacts), {
+        headers: new HttpHeaders().set('Content-Type', 'application/json'),
+      })
+      .subscribe(() => {
+        this.contacts.sort((a, b) => {
+          if (a < b) return -1;
+          if (a > b) return 1;
+          return 0;
+        });
+        this.contactListChangedEvent.next(this.contacts.slice());
+      });
   }
 
   addContact(newContact: Contact) {
@@ -47,18 +56,42 @@ export class ContactService {
     this.maxContactId++;
     newContact.id = `${this.maxContactId}`;
     this.contacts.push(newContact);
-    this.contactListChangedEvent.next(this.contacts.slice());
+    this.storeContacts();
+  }
+
+  getContact(id: string): Contact | undefined {
+    return this.contacts.find((c) => c.id === id);
   }
 
   updateContact(original: Contact, newContact: Contact) {
-    if (!original || !newContact) {
+    if (
+      newContact === null ||
+      newContact === undefined ||
+      original === null ||
+      original === undefined
+    ) {
       return;
     }
     const pos = this.contacts.indexOf(original);
     if (pos < 0) return;
-
     newContact.id = original.id;
     this.contacts[pos] = newContact;
-    this.contactListChangedEvent.next(this.contacts.slice());
+    this.storeContacts();
+  }
+
+  deleteContact(contact: Contact) {
+    if (!contact) return;
+    const pos = this.contacts.indexOf(contact);
+    if (pos < 0) return;
+    this.contacts.splice(pos, 1);
+    this.storeContacts();
+  }
+
+  getMaxId(): number {
+    let maxId = 0;
+    this.contacts.forEach((c) => {
+      if (+c.id > maxId) maxId = +c.id;
+    });
+    return maxId;
   }
 }
