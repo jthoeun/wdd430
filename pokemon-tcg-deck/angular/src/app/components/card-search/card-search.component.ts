3afcda843eecfc878aeaccb3fc541674b45371de
jthoeun <johnny.thoeun@gmail.com>
2025-07-14 21:43:18 -0600

    Pokemon TCG Deck App Inital Commit & Set up

---
+++ pokemon-tcg-deck/angular/src/app/components/card-search/card-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-search',
+  imports: [],
+  templateUrl: './card-search.component.html',
+  styleUrl: './card-search.component.css'
+})
+export class CardSearchComponent {
+
+}
